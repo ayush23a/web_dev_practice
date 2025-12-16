6cfbca5ec7c590509ad222b03fc9c05f3c295791
Ayush Aman <140056729+ayush23a@users.noreply.github.com>
2025-12-16 13:41:08 +0000

    comparision learned

---
+++ Javascript/comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+
+
+// console.log("2" > 1); // true (first conversion then comparision)
+// console.log("02" > 1); // true "" 
+
+// console.log(null > 0);
+// console.log(null == 0); 
+// console.log(null >= 0);
+
+// equality check (==) and comparision opr works differently 
+// comparision opr first converts null into num then it compares with 0
+// thats why the values are diff
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// strict check ===
+// check for the datatype also along with the value
+
+console.log("2" === 2); // false ( datatype not same)
