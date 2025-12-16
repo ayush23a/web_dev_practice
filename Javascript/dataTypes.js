86c1bacee23f5f66f51041b967f297ed2d9db8ba
Ayush Aman <140056729+ayush23a@users.noreply.github.com>
2025-12-16 12:31:10 +0000

    datatypes

---
+++ Javascript/dataTypes.js
@@ -0,0 +1,24 @@
+let age = 18
+let name = "ayush"
+let isLoggedIn = true
+let State;
+
+// different data types
+// number => max upto 2^53
+// bigint => for larger nombers
+// boolean => true/false
+// string => ""
+// null => standalone value
+// undefined => which is not defined
+// symbol => to identify uniqueness (mostly used in frameworks like react, vue etc)
+// object
+
+
+//console.log(typeof isLoggedIn);
+console.log(typeof undefined); // type -> undefined
+console.log(typeof null); // type -> object
+
+
+// read MDN docs and ECMA standards for more refs 
+
+
